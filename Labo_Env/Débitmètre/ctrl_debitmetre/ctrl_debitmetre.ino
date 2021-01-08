f140ee4c4384c4ed9182fbaaae7d840fd08d6da6
Émile Jetzer <emile.jetzer@polymtl.ca>
2021-01-08 09:27:56 -0500

    Interface minimale fonctionnelle pour une carte Arduino Due.

--- Labo_Env/Débitmètre/ctrl_debitmetre/ctrl_debitmetre.ino
+++ Labo_Env/Débitmètre/ctrl_debitmetre/ctrl_debitmetre.ino
@@ -8,12 +8,15 @@
 
 // Inclusions
 #define BROCHE 2 // La broche 2 permet les interruptions[^2]
+#define ENVOI_AUTOMATIQUE false // Permet de régler l'envoi automatique des données à un intervalle.
 
 // Variables globales
 volatile long unsigned int nombre_de_tours = 0; // Nombre de tours dans les derniers delai ms
 unsigned int delai = 1000; // [ms] Délai entre les calculs de débit
+unsigned int delai_com = 2000; // [ms] Délai minimal entre les communications
 float debit = 0; // [Hz]
 long unsigned int derniere_mesure = 0; // [ms] Moment de la dernière mesure
+long unsigned int derniere_com = 0; // [ms] Moment de la dernière vérification de communication
 
 void setup() {
   // Intialiser la communication série
@@ -26,26 +29,34 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(BROCHE), interruption, RISING);
 
   // Attente pour la communication série...
-  while (!Serial) {
-    ;
-  }
+  while (!Serial) {;}
 
   // Annonce des colonnes
   Serial.println("t[s] f[Hz]");
 }
 
 void loop() {
-  // Est-ce qu'il y a une demande d'information?
-  if (Serial.available()) {
-    detachInterrupt(digitalPinToInterrupt(BROCHE));
-    String commande = Serial.readStringUntil('\n'); // [^1]
-    Serial.println(commande);
-
-    if (commande == "lire") {
+  // On vérifie périodiquement si on a une demande de communication,
+  // mais pas trop souvent: ça interfère avec l'exécution des interruptions.
+  if ( (millis() - derniere_com) >= delai_com ) {
+    // Est-ce qu'il y a une demande d'information?
+    if (Serial.available()) {
+      detachInterrupt(digitalPinToInterrupt(BROCHE));
+      String commande = Serial.readStringUntil('\n'); // [^1]
+      Serial.println(commande);
+  
+      if (commande == "lire") {
+        float heure = (float)derniere_mesure / 1000.0; // [s]
+        Serial.println(String(heure) + " " + String(debit));
+      }
+      attachInterrupt(digitalPinToInterrupt(BROCHE), interruption, RISING);
+    } else if (ENVOI_AUTOMATIQUE) {
+      detachInterrupt(digitalPinToInterrupt(BROCHE));
       float heure = (float)derniere_mesure / 1000.0; // [s]
       Serial.println(String(heure) + " " + String(debit));
+      attachInterrupt(digitalPinToInterrupt(BROCHE), interruption, RISING);
     }
-    attachInterrupt(digitalPinToInterrupt(BROCHE), interruption, RISING);
+    derniere_com = millis();
   }
 
   // Mise à jour des lectures
